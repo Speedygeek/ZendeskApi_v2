1156c307035d504b5bf999856bb100cc3a361ea8
Eric Neifert <eric.neifert@gmail.com>
2012-11-06 08:19:12 -0500

    renaming to Zendesk

---
+++ ZenDeskApi_v2/ZendeskApi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using RestSharp;
+using RestSharp.Contrib;
+using ZenDeskApi_v2.Requests;
+
+namespace ZenDeskApi_v2
+{
+    public class ZendeskApi
+    {
+        public Tickets Tickets { get; set; }
+        public Attachments Attachments { get; set; }
+        public Views Views { get; set; }
+        public Users Users { get; set; }
+        public Requests.Requests Requests { get; set; }
+
+        public ZendeskApi(string yourZenDeskUrl, string user, string password)
+        {
+            Tickets = new Tickets(yourZenDeskUrl, user, password);
+            Attachments = new Attachments(yourZenDeskUrl, user, password);
+            Views = new Views(yourZenDeskUrl, user, password);
+            Users = new Users(yourZenDeskUrl, user, password);
+            Requests = new Requests.Requests(yourZenDeskUrl, user, password);
+        }
+    }
+}
