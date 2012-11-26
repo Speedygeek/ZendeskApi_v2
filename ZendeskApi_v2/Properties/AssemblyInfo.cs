7b1531fc4a8aa4423bfb2a27efda39150d46a284
Eric Neifert <eric.neifert@gmail.com>
2012-11-25 21:15:42 -0500

    enabled package restore

--- ZendeskApi_v2/Properties/AssemblyInfo.cs
+++ ZendeskApi_v2/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZendeskApi_v2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A full c# wrapper for Zendesk's api v2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eric Neifert")]
 [assembly: AssemblyProduct("ZendeskApi_v2")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
