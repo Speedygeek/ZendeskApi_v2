b963e073adcff60fb84d5541bb633b7eb8ed844a
Eric Neifert <eric.neifert@gmail.com>
2012-12-03 16:34:40 -0500

    added some async tests

--- Tests/TicketTests.cs
+++ Tests/TicketTests.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using ZendeskApi_v2;
 using ZendeskApi_v2.Models.Constants;
@@ -17,6 +18,41 @@ namespace Tests
     public class TicketTests
     {        
         ZendeskApi api = new ZendeskApi(Settings.Site, Settings.Email, Settings.Password);
+
+        [Test]
+        public void CanGetTicketsAsync()
+        {
+            var tickets = api.Tickets.GetAllTicketsAsync();
+            Assert.True(tickets.Result.Count > 0);
+        }
+
+        [Test]
+        public void CanCreateUpdateAndDeleteTicketAsync()
+        {
+            var ticket = new Ticket()
+            {
+                Subject = "my printer is on fire",
+                Description = "HELP",
+                Priority = TicketPriorities.Urgent
+            };
+
+            var res = api.Tickets.CreateTicketAsync(ticket).Result.Ticket;
+
+            Assert.NotNull(res);
+            Assert.Greater(res.Id, 0);
+
+            res.Status = TicketStatus.Solved;
+            res.AssigneeId = Settings.UserId;
+
+            res.CollaboratorEmails = new List<string>() { Settings.ColloboratorEmail };
+            var body = "got it thanks";
+            var updateResponse = api.Tickets.UpdateTicketAsync(res, new Comment() { Body = body, Public = true });
+
+            Assert.NotNull(updateResponse.Result);
+            Assert.AreEqual(updateResponse.Result.Audit.Events.First().Body, body);
+
+            Assert.True(api.Tickets.DeleteAsync(res.Id).Result);
+        }
 
         [Test]
         public void CanGetTickets()
