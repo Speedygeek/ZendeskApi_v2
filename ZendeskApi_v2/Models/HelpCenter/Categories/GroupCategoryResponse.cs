89ace6437f1f5b4464d1fe67794ad5ef7521af6c
peteallen <peteallen@gmail.com>
2014-10-10 11:30:18 -0600

    Add HelpCenter Category models

---
+++ ZendeskApi_v2/Models/HelpCenter/Categories/GroupCategoryResponse.cs
@@ -0,0 +1,18 @@
+﻿// JSON C# Class Generator
+// http://at-my-window.blogspot.com/?page=json-class-generator
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ZendeskApi_v2.Models.HelpCenter.Categories
+{
+
+   public class GroupCategoryResponse : GroupResponseBase
+   {
+
+      [JsonProperty("categories")]
+      public IList<Category> Categories { get; set; }
+   }
+}
---
+++ ZendeskApi_v2/Models/HelpCenter/Categories/IndividualCategoryResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.HelpCenter.Categories
+{
+   public class IndividualCategoryResponse
+   {
+
+      [JsonProperty("category")]
+      public Category Category { get; set; }
+   }
+}
