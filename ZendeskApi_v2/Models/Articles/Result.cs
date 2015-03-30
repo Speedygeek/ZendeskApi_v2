9500bc382087e4bf7ea29ea152d89bab24a3db00
Steve McNiven-Scott <steve@sitefinitysteve.com>
2015-03-30 18:58:17 -0400

    Added HelpCenter Article Search DTOs

---
+++ ZendeskApi_v2/Models/Articles/Result.cs
@@ -0,0 +1,76 @@
+﻿// JSON C# Class Generator
+// http://at-my-window.blogspot.com/?page=json-class-generator
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using ZendeskApi_v2.Models.Shared;
+using ZendeskApi_v2.Models.Tickets;
+
+namespace ZendeskApi_v2.Models.Articles {
+    public class Result
+    {
+        
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonProperty("author_id")]
+        public int AuthorId { get; set; }
+
+        [JsonProperty("comments_disabled")]
+        public bool CommentsDisabled { get; set; }
+
+        [JsonProperty("draft")]
+        public bool Draft { get; set; }
+
+        [JsonProperty("promoted")]
+        public bool Promoted { get; set; }
+
+        [JsonProperty("position")]
+        public int Position { get; set; }
+
+        [JsonProperty("vote_sum")]
+        public int VoteSum { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonProperty("section_id")]
+        public int SectionId { get; set; }
+
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public string UpdatedAt { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        [JsonProperty("source_locale")]
+        public string SourceLocale { get; set; }
+
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
+
+        [JsonProperty("outdated")]
+        public bool Outdated { get; set; }
+
+        [JsonProperty("result_type")]
+        public string ResultType { get; set; }
+    }
+}
