92eeb9c1f7184b833c501bb838f861cfab30cc36
Matt Nisnevich <mattnis@gmail.com>
2015-08-21 11:32:38 -0500

    Surround using System.Threading.Tasks with #if ASYNC

--- ZendeskApi_v2/Requests/Voice.cs
+++ ZendeskApi_v2/Requests/Voice.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
+#if ASYNC
 using System.Threading.Tasks;
+#endif
 
 namespace ZendeskApi_v2.Requests
 {
