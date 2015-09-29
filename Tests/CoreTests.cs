b9d61e2299ccc26b760966a13470a2d458edcf92
dcrowe <crowe.daniel@gmail.com>
2015-09-29 10:28:41 +0930

    Revert "Issue #165 Fixed null-ptr error in RunRequest; Fixed incorrect except…"

--- Tests/CoreTests.cs
+++ Tests/CoreTests.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
-using ZendeskApi_v2.Models.Tickets;
 
 namespace Tests
 {
@@ -41,24 +38,6 @@ namespace Tests
 
             Assert.NotNull(ticket);
             Assert.AreEqual(ticket.Id, id);
-        }
-
-        [Test]
-        public void AsyncGivesCorrectException()
-        {
-            var api = new ZendeskApi_v2.ZendeskApi(
-                "http://csharpapi.zendesk.com/api/v2", 
-                Settings.Email, 
-                "Incorrect password");
-
-            Assert.Throws<WebException>(async () =>
-            {
-                await api.Tickets.CreateTicketAsync(new Ticket
-                {
-                    Subject = "subject"
-                });
-            });
-
         }
     }
 }
