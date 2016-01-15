a8dd50eaa16f6aeba03e53ce1f8e5b7e9afdf23a
oooooh <caiyuyong@outlook.com>
2016-01-15 16:38:22 +0800

    Add test case for fetch translation metadata capability when getting locales.

--- Tests/LocaleTests.cs
+++ Tests/LocaleTests.cs
@@ -19,9 +19,19 @@ namespace Tests
 
             var specific = api.Locales.GetLocaleById(all.Locales[0].Id);
             Assert.AreEqual(specific.Locale.Id, all.Locales[0].Id);
+            Assert.IsNull(specific.Locale.Translations);
+
+            var specificWithTranslation = api.Locales.GetLocaleById(all.Locales[0].Id, true);
+            Assert.AreEqual(specificWithTranslation.Locale.Id, all.Locales[0].Id);
+            Assert.IsNotNull(specificWithTranslation.Locale.Translations);
 
             var current = api.Locales.GetCurrentLocale();
             Assert.Greater(current.Locale.Id, 0);
+            Assert.IsNull(current.Locale.Translations);
+
+            var currentWithTranslation = api.Locales.GetCurrentLocale(true);
+            Assert.Greater(currentWithTranslation.Locale.Id, 0);
+            Assert.IsNotNull(currentWithTranslation.Locale.Translations);
         }
     }
 }
