dde0da74ce2b02ee4d8934f4203a7c3d0dd2756b
mgressman <mgressman@brightwavepartners.com>
2016-01-25 21:49:01 -0600

    Refactor the incremental ticket export to include correct url and proper paging

--- ZendeskApi_v2/Constants/ResourceConstants.cs
+++ ZendeskApi_v2/Constants/ResourceConstants.cs
@@ -2,12 +2,9 @@
 {
     public static class ResourceConstants
     {
-        #region Properties
-
         public static string GetIncrementalTicketExportResource
         {
             get { return "incremental/tickets.json?start_time="; }
         }
-        #endregion
     }
 }
