0899cfd3fcc762f368fccf839fbb78682b46a75f
Marcus10110 <markgarrison@saleae.com>
2016-04-27 20:20:21 -0700

    Added Async Help Center Translation functions and tests.
    The section translation test fails becuase I forgot to fix the async section function first. This will pass once that's merged in.

--- src/ZendeskApi_v2/Requests/HelpCenter/Translations.cs
+++ src/ZendeskApi_v2/Requests/HelpCenter/Translations.cs
@@ -38,6 +38,30 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 #endif
 
 #if ASYNC
+
+		Task<GroupTranslationResponse> ListTranslationsForArticleAsync( long articleId );
+		Task<GroupTranslationResponse> ListTranslationsForSectionAsync( long articleId );
+		Task<GroupTranslationResponse> ListTranslationsForCategoryAsync( long articleId );
+
+		Task<IList<string>> ListMissingTranslationsForArticleAsync( long articleId );
+		Task<IList<string>> ListMissingTranslationsForSectionAsync( long articleId );
+		Task<IList<string>> ListMissingTranslationsForCategoryAsync( long articleId );
+
+		Task<IndividualTranslationResponse> ShowTranslationForArticleAsync( long articleId, string locale );
+
+		Task<IndividualTranslationResponse> CreateArticleTranslationAsync( long articleId, Translation translation );
+		Task<IndividualTranslationResponse> CreateSectionTranslationAsync( long SectionId, Translation translation );
+		Task<IndividualTranslationResponse> CreateCategoryTranslationAsync( long CategoryId, Translation translation );
+
+		Task<IndividualTranslationResponse> UpdateArticleTranslationAsync( Translation translation );
+		Task<IndividualTranslationResponse> UpdateSectionTranslationAsync( Translation translation );
+		Task<IndividualTranslationResponse> UpdateCategoryTranslationAsync( Translation translation );
+
+		Task<bool> DeleteTranslationAsync( long translationId );
+
+		Task<Tuple<IList<string>, string>> ListAllEnabledLocalesAndDefaultLocaleAsync();
+
+
 #endif
 	}
 
@@ -150,6 +174,104 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 #endif
 
 #if ASYNC
+
+
+		public async Task<GroupTranslationResponse> ListTranslationsForArticleAsync( long articleId )
+		{
+			var resourceUrl = String.Format( "/help_center/articles/{0}/translations.json", articleId );
+
+			return await GenericGetAsync<GroupTranslationResponse>( resourceUrl );
+		}
+
+		public async Task<GroupTranslationResponse> ListTranslationsForSectionAsync( long articleId )
+		{
+			var resourceUrl = String.Format( "/help_center/sections/{0}/translations.json", articleId );
+
+			return await GenericGetAsync<GroupTranslationResponse>( resourceUrl );
+		}
+
+		public async Task<GroupTranslationResponse> ListTranslationsForCategoryAsync( long articleId )
+		{
+			var resourceUrl = String.Format( "/help_center/categories/{0}/translations.json", articleId );
+
+			return await GenericGetAsync<GroupTranslationResponse>( resourceUrl );
+		}
+
+
+		public async Task<IList<string>> ListMissingTranslationsForArticleAsync( long articleId )
+		{
+			var res = RunRequestAsync( String.Format( "help_center/articles/{0}/translations/missing.json", articleId ), "GET" );
+			var anon_type = new { locales = new List<string>() };
+			var locale_info = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType( res.Result.Content, anon_type );
+			return locale_info.locales;
+		}
+		public async Task<IList<string>> ListMissingTranslationsForSectionAsync( long articleId )
+		{
+			var res = RunRequestAsync( String.Format( "help_center/sections/{0}/translations/missing.json", articleId ), "GET" );
+			var anon_type = new { locales = new List<string>() };
+			var locale_info = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType( res.Result.Content, anon_type );
+			return locale_info.locales;
+		}
+		public async Task<IList<string>> ListMissingTranslationsForCategoryAsync( long articleId )
+		{
+			var res = RunRequestAsync( String.Format( "help_center/categories/{0}/translations/missing.json", articleId ), "GET" );
+			var anon_type = new { locales = new List<string>() };
+			var locale_info = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType( res.Result.Content, anon_type );
+			return locale_info.locales;
+		}
+
+		public async Task<IndividualTranslationResponse> ShowTranslationForArticleAsync( long articleId, string locale )
+		{
+			var resourceUrl = String.Format( "/help_center/articles/{0}/translations/{1}.json", articleId, locale );
+			return await GenericGetAsync<IndividualTranslationResponse>( resourceUrl );
+		}
+
+		public async Task<IndividualTranslationResponse> CreateArticleTranslationAsync( long articleId, Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPostAsync<IndividualTranslationResponse>( string.Format( "help_center/articles/{0}/translations.json", articleId ), body );
+		}
+		public async Task<IndividualTranslationResponse> CreateSectionTranslationAsync( long SectionId, Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPostAsync<IndividualTranslationResponse>( string.Format( "help_center/sections/{0}/translations.json", SectionId ), body );
+		}
+		public async Task<IndividualTranslationResponse> CreateCategoryTranslationAsync( long CategoryId, Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPostAsync<IndividualTranslationResponse>( string.Format( "help_center/categories/{0}/translations.json", CategoryId ), body );
+		}
+
+		public async Task<IndividualTranslationResponse> UpdateArticleTranslationAsync( Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPutAsync<IndividualTranslationResponse>( string.Format( "help_center/articles/{0}/translations/{1}.json", translation.SourceId, translation.Locale ), body );
+		}
+
+		public async Task<IndividualTranslationResponse> UpdateSectionTranslationAsync( Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPutAsync<IndividualTranslationResponse>( string.Format( "help_center/sections/{0}/translations/{1}.json", translation.SourceId, translation.Locale ), body );
+		}
+
+		public async Task<IndividualTranslationResponse> UpdateCategoryTranslationAsync( Translation translation )
+		{
+			var body = new { translation };
+			return await GenericPutAsync<IndividualTranslationResponse>( string.Format( "help_center/categories/{0}/translations/{1}.json", translation.SourceId, translation.Locale ), body );
+		}
+
+		public async Task<bool> DeleteTranslationAsync( long translationId )
+		{
+			return await GenericDeleteAsync( string.Format( "help_center/translations/{0}.json", translationId ) );
+		}
+
+		public async Task<Tuple<IList<string>, string>> ListAllEnabledLocalesAndDefaultLocaleAsync( )
+		{
+			var res = await RunRequestAsync( "help_center/locales.json", "GET" );
+			var anon_type = new { locales = new List<string>(), default_locale = "" };
+			var locale_info = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType( res.Content, anon_type );
+			return new Tuple<IList<string>, string>( locale_info.locales, locale_info.default_locale );
+		}
 #endif
 
 
