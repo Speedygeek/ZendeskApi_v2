b86a6fb69352865eff7979c802c65bdee1d6d2a9
Mark <markgarrison@saleae.com>
2016-05-18 20:22:29 -0700

    Follow up to Help Center Section Fix (#217)
    * added 2 section unit tests. Update fails as expected.
    
    * Fixed CreateSection API call
    Updated Section unit test to include category ID
    
    * fixed async support and added async tests for Help Center Section functions.

--- src/Tests/HelpCenter/SectionTests.cs
+++ src/Tests/HelpCenter/SectionTests.cs
@@ -12,7 +12,6 @@ namespace Tests.HelpCenter
 		private ZendeskApi api = new ZendeskApi( Settings.Site, Settings.Email, Settings.Password );
 
 		[Test]
-
 		public void CanGetSections()
 		{
 			var res = api.HelpCenter.Sections.GetSections();
@@ -40,6 +39,37 @@ namespace Tests.HelpCenter
 			Assert.AreEqual( update.Section.Description, res.Section.Description );
 
 			Assert.True( api.HelpCenter.Sections.DeleteSection( res.Section.Id.Value ) );
+		}
+
+
+		[Test]
+		public void CanGetSectionsAsync()
+		{
+			var res = api.HelpCenter.Sections.GetSectionsAsync().Result;
+			Assert.Greater( res.Count, 0 );
+
+			var res1 = api.HelpCenter.Sections.GetSectionById( res.Sections[ 0 ].Id.Value );
+			Assert.AreEqual( res1.Section.Id, res.Sections[ 0 ].Id.Value );
+		}
+
+		[Test]
+		public void CanCreateUpdateAndDeleteSectionsAsync()
+		{
+			//https://csharpapi.zendesk.com/hc/en-us/categories/200382245-Category-1
+			long category_id = 200382245;
+
+			var res = api.HelpCenter.Sections.CreateSectionAsync( new Section()
+			{
+				Name = "My Test section",
+				CategoryId = category_id
+			} ).Result;
+			Assert.Greater( res.Section.Id, 0 );
+
+			res.Section.Description = "updated description";
+			var update = api.HelpCenter.Sections.UpdateSectionAsync( res.Section ).Result;
+			Assert.AreEqual( update.Section.Description, res.Section.Description );
+
+			Assert.True( api.HelpCenter.Sections.DeleteSectionAsync( res.Section.Id.Value ).Result );
 		}
 	}
 }
