0d43183f5e6d209c47a8e59b2e6d0248fcae4ccc
WinDrop <Buxarik88@gmail.com>
2016-07-13 17:23:05 +0100

    Added Data Class allowing to pull out voice data with Ticket Audit Event. Added GetAuditsNextPage method to tickets.cs allowing to request information for tickets having more than 100 audits.

---
+++ src/ZendeskApi_v2/Models/Shared/Data.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json;
+using ZendeskApi_v2.Models.Tickets;
+using Newtonsoft.Json.Converters;
+
+namespace ZendeskApi_v2.Models.Shared
+{
+    public class Data
+    {
+        [JsonProperty("from")]
+        public string from { get; set; }
+
+        [JsonProperty("to")]
+        public string to { get; set; }
+
+        [JsonProperty("recording_url")]
+        public string recording_url { get; set; }
+
+        [JsonProperty("call_id")]
+        public long call_id { get; set; }
+
+        [JsonProperty("call_duration")]
+        public int call_duration { get; set; }
+
+        [JsonProperty("answered_by_id")]
+        public long? answered_by_id { get; set; }
+
+        [JsonProperty("started_at")]
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTimeOffset? started_at { get; set; }
+
+        [JsonProperty("")]
+        public string MyProperty { get; set; }
+
+        [JsonProperty("location")]
+        public string location { get; set; }
+
+        [JsonProperty("author_id")]
+        public long? author_id { get; set; }
+
+        [JsonProperty("public")]
+        public bool? Public { get; set; }
+
+        [JsonProperty("brand_id")]
+        public long brand_id { get; set; }
+
+        [JsonProperty("via_id")]
+        public int via_id { get; set; }
+
+        [JsonProperty("answered_by_name")]
+        public string answered_by_name { get; set; }
+
+        [JsonProperty("transcription_status")]
+        public string transcription_status { get; set; }
+    }
+}
