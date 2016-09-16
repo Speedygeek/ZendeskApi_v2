3efd572da2f0986b114e4e6d83adefbe88443728
mozts2005 <mozts2005@gmail.com>
2016-09-15 22:57:15 -0600

    fixes #238 made SortOrderAscending option now work with asc and ascending

--- src/Tests/SearchTests.cs
+++ src/Tests/SearchTests.cs
@@ -14,65 +14,6 @@ namespace Tests
     public class SearchTests
     {
         private ZendeskApi api = new ZendeskApi(Settings.Site, Settings.Email, Settings.Password);
-         /*     
-        [TestFixtureSetUp]
-        public void Init()
-        {
-            const int range = 3; 
-            var r = new Random();
-            var number = 0;
-            var typesOfFeedback = new[] { "Effective", "Descriptive", "Evaluative ", "Motivational" };
-            for (var i = 0; i < 100; i++)
-            {
-                var ticket = new Ticket
-                {
-                    Subject = string.Format("my printer is on fire - {0}", i),
-                    Comment = new Comment()
-                    {
-                        Body = "HELP"
-                    },
-                    Priority = TicketPriorities.Urgent,
-                    CustomFields = new List<CustomField>()
-                    {
-                        new CustomField()
-                        {
-                            Id = Settings.CustomFieldTypeOfFeedbackId,
-                            Value = typesOfFeedback[number]
-                        },
-                    },
-                };
-
-                var res = api.Tickets.CreateTicket(ticket).Ticket;
-
-            }
-
-            for (int i = 0; i < 150; i++)
-            {
-                number = r.Next(range);
-
-                var ticket = new Ticket
-                {
-                    Subject = string.Format("my printer is on fire - {0}", i),
-                    Comment = new Comment()
-                    {
-                        Body = "HELP"
-                    },
-                    Priority = TicketPriorities.Urgent,
-                    CustomFields = new List<CustomField>()
-                    {
-                        new CustomField()
-                        {
-                            Id = Settings.CustomFieldTypeOfFeedbackId,
-                            Value = typesOfFeedback[number]
-                        },
-                    },
-                };
-
-                var res = api.Tickets.CreateTicket(ticket).Ticket;
-
-            }
-        }
-         */
 
         [Test]
         public void CanSearch()
@@ -85,7 +26,7 @@ namespace Tests
         [Test]
         public void BackwardCompatibilitAfterAddingPagination()
         {
-            var res = api.Search.SearchFor("Effective", "created_at","asc");
+            var res = api.Search.SearchFor("Effective", "created_at", "asc");
             Assert.IsTrue(res.Count > 0);
         }
         [Test]
@@ -94,22 +35,22 @@ namespace Tests
             var res = api.Search.SearchFor("Effective"); //search for a custom field - the results are more than one page
             var total = res.Count;
 
-            Assert.IsTrue(res.Count >0); 
+            Assert.IsTrue(res.Count > 0);
             Assert.IsTrue(res.Count > res.Results.Count); //result has more than one page
             Assert.IsTrue(!string.IsNullOrEmpty(res.NextPage)); //It has next page
 
-            res = api.Search.SearchFor("Effective","","",2); //fetch next page
+            res = api.Search.SearchFor("Effective", "", "", 2); //fetch next page
             Assert.IsTrue(res.Count > 0);
-            Assert.IsTrue(res.Count==total); //number of results should be same as page 1
-            
+            Assert.IsTrue(res.Count == total); //number of results should be same as page 1
+
         }
         [Test]
         public void TicketHasSubject()
         {
             var res = api.Search.SearchFor("my printer is on fire");
 
-            Assert.IsTrue(res!=null);
-            Assert.IsTrue(res.Results.Count>0);
+            Assert.IsTrue(res != null);
+            Assert.IsTrue(res.Results.Count > 0);
             Assert.IsTrue(!string.IsNullOrEmpty(res.Results[0].Subject));
         }
 
@@ -177,6 +118,18 @@ namespace Tests
             Assert.AreEqual(res.Results.Count, 1);
             Assert.AreEqual(res.Results[0].Id, Settings.UserId);
             Assert.IsTrue(res.Results[0] is User);
+        }
+
+        [Test]
+        public void SearchSortIsWorking()
+        {
+            //desc asc 
+            var res = api.Search.SearchFor<Ticket>("Effective", "created_at", "asc");
+            Assert.IsTrue(res.Count > 2);
+            var first = res.Results[0];
+            var second = res.Results[1];
+            Assert.That(second.CreatedAt, Is.GreaterThan(first.CreatedAt));
+
         }
     }
 }
