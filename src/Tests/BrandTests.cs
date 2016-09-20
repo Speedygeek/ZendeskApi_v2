fb4d29da405571c038810d3019351061ef41199e
Jeremy Smith <jsmith@certipay.com>
2016-09-20 18:05:54 -0400

    Added unit tests.

---
+++ src/Tests/BrandTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ZendeskApi_v2;
+using ZendeskApi_v2.Models.Brands;
+
+namespace Tests
+{
+    [TestFixture]
+    public class BrandTests
+    {
+        private ZendeskApi api = new ZendeskApi(Settings.Site, Settings.Email, Settings.Password);
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var brands = api.Brands.GetBrands();
+            if (brands != null)
+            {
+                foreach (var brand in brands.Brands.Where(o => o.Name.Contains("Test Brand")))
+                {
+                    api.Brands.DeleteBrand(brand.Id.Value);
+                }
+            }
+        }
+
+        [Test]
+        public void CanGetBrands()
+        {
+            var res = api.Brands.GetBrands();
+            Assert.Greater(res.Count, 0);
+
+            var ind = api.Brands.GetBrand(res.Brands[0].Id.Value);
+            Assert.AreEqual(ind.Brand.Id, res.Brands[0].Id);            
+        }
+
+        [Test]
+        public void CanCreateUpdateAndDeleteTriggers()
+        {
+            var brand = new Brand()
+            {
+                Name = "Test Brand",
+                Active = true,
+                Subdomain = "testdomain"
+            };
+
+            var res = api.Brands.CreateBrand(brand);
+
+            Assert.Greater(res.Brand.Id, 0);
+
+            res.Brand.Name = "Test Brand Updated";
+            var update        = api.Brands.UpdateBrand(res.Brand);
+            Assert.AreEqual(update.Brand.Name, res.Brand.Name);
+
+            Assert.True(api.Brands.DeleteBrand(res.Brand.Id.Value));
+        }
+      
+    }
+}
