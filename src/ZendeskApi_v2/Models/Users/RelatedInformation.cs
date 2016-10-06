dd7e06048b4b9031bd7758a190467cb713605542
M3LiNdRu <rogercalaf@gmail.com>
2016-10-06 20:16:07 +0200

    Added two new operations for getting user related info

---
+++ src/ZendeskApi_v2/Models/Users/RelatedInformation.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZendeskApi_v2.Models.Users
+{
+    public class RelatedInformation
+    {
+
+        [JsonProperty("assigned_tickets")]
+        public int AssignedTickets { get; set; }
+     
+        [JsonProperty("requested_tickets")]
+        public int RequestedTickets { get; set; }
+
+        [JsonProperty("ccd_tickets")]
+        public int CcdTickets { get; set; }
+
+        [JsonProperty("topics")]
+        public int Topics { get; set; }
+
+        [JsonProperty("topic_comments")]
+        public int TopicComments { get; set; }
+
+        [JsonProperty("votes")]
+        public int Votes { get; set; }
+
+        [JsonProperty("subscriptions")]
+        public int Subscriptions { get; set; }
+
+        [JsonProperty("entry_subscriptions")]
+        public int EntrySubscriptions { get; set; }
+
+        [JsonProperty("forum_subscriptions")]
+        public int FormSubscriptions { get; set; }
+ 
+        [JsonProperty("organization_subscriptions")]
+        public int OrganitzationSubscriptions { get; set; }
+    }
+}
