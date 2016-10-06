2b655143ffddb5a23eb697c484cb32907f97fff3
M3LiNdRu <rogercalaf@gmail.com>
2016-10-06 20:16:07 +0200

    Added two new operations for getting user related info

---
+++ src/ZendeskApi_v2/Models/Users/IndividualUserRelatedInformationResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZendeskApi_v2.Models.Users
+{
+    public class IndividualUserRelatedInformationResponse
+    {
+        [JsonProperty("user_related")]
+        public RelatedInformation UserRelated { get; set; }
+    }
+}
