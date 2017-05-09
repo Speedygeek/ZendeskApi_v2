e7f96e3ff520de7fdc19e544a8c096a695e870eb
WinDrop <Buxarik88@gmail.com>
2017-05-09 21:58:03 +0100

    Update TicketTests.cs

--- src/Tests/TicketTests.cs
+++ src/Tests/TicketTests.cs
@@ -540,7 +540,7 @@ namespace Tests
         public void CanCreateTicketWithDueDate()
         {
             //31 December 2020 2AM
-            var dueAt = DateTimeOffset.Parse("31/12/2020 07:00:00 -05:00");
+            var dueAt = DateTimeOffset.Parse("12/31/2020 07:00:00 -05:00");
 
             var ticket = new Ticket()
             {
