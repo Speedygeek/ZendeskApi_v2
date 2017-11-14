7b71ee9b756b1c8b93155cf10422421259ca5457
Elizabeth Schneider <elizabeth@speedy-geek.com>
2017-11-14 00:07:39 -0700

    Update to fix testing

--- test/ZendeskApi_v2.Test/GlobalSuppressions.cs
+++ test/ZendeskApi_v2.Test/GlobalSuppressions.cs
@@ -3,5 +3,7 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+[assembly: NUnit.Framework.LevelOfParallelism(3)]
+[assembly: NUnit.Framework.Parallelizable(NUnit.Framework.ParallelScope.Fixtures)]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SEC0112:Path Tampering Unvalidated File Path", Justification = "Unit Test", Scope = "type", Target = "Tests.UserTests")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SEC0112:Path Tampering Unvalidated File Path", Justification = "Unit Test", Scope = "type", Target = "Tests.TicketTests")]
