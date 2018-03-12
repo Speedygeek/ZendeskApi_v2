d7e949f40e17ee291e6c3b2b69a66f5fdfdf0026
Elizabeth Schneider <mozts2005@gmail.com>
2018-03-12 15:29:21 -0600

    updated unit test (#326)
    * updated unit test
    updated release notes for 3.8
    
    * update the build for TLS 1.2 for Github
    
    * disabled dotnet core testing
    
    * updated CLA system

--- tools/ReleaseNotes.ps1
+++ tools/ReleaseNotes.ps1
@@ -1,9 +1,9 @@
 Param(
-  [string]$Version = "3.7.2",
+  [string]$Version = "3.8.0",
   [string]$gitHubToken = "$env:GitHubToken"
 )
 
 $gitReleaseNotesPath = "$PSScriptRoot\GitReleaseNotes.exe"
 Push-Location  "$PSScriptRoot\.."
-& $gitReleaseNotesPath . /O ReleaseNotes.md /Vers $Version /RepoUrl https://github.com/mozts2005/ZendeskApi_v2 /RepoUsername mozts2005 /RepoT $gitHubToken /AllT /AllL /IssueTrackerUsername mozts /IssueTrackerToken $gitHubToken
+& $gitReleaseNotesPath . /O ReleaseNotes.md /Vers $Version /RepoUrl https://github.com/mozts2005/ZendeskApi_v2 /RepoUsername mozts2005 /RepoT $gitHubToken /IssueTrackerUsername mozts /IssueTrackerToken $gitHubToken
 Pop-Location
