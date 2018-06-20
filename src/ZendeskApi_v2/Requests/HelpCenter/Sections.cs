e739fc9fb40a1dfaec38cffdfa81e613d82558df
Denis P <lsden1995@gmail.com>
2018-06-20 13:03:49 +0300

    Add locale to categories (#336)
    * Add locale to categories
    
    * Change check for localization been added
    
    * Add more locale based stuff

--- src/ZendeskApi_v2/Requests/HelpCenter/Sections.cs
+++ src/ZendeskApi_v2/Requests/HelpCenter/Sections.cs
@@ -33,37 +33,44 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 
     public class Sections : Core, ISections
     {
-        public Sections(string yourZendeskUrl, string user, string password, string apiToken, string p_OAuthToken)
+        private string Locale { get; }
+
+        private string GeneralSectionsPath => string.IsNullOrWhiteSpace(Locale)
+            ? "help_center/sections"
+            : $"help_center/{Locale}/sections";
+
+        public Sections(string yourZendeskUrl, string user, string password, string apiToken, string locale, string p_OAuthToken)
             : base(yourZendeskUrl, user, password, apiToken, p_OAuthToken)
         {
+            Locale = locale;
         }
 
 #if SYNC
 
         public GroupSectionResponse GetSections()
         {
-            return GenericGet<GroupSectionResponse>("help_center/sections.json?include=access_policies");
+            return GenericGet<GroupSectionResponse>($"{GeneralSectionsPath}.json?include=access_policies");
         }
         public GroupSectionResponse GetSectionsByCategoryId(long categoryId)
         {
-            return GenericGet<GroupSectionResponse>($"help_center/categories/{categoryId}/sections.json?include=access_policies");
+            return GenericGet<GroupSectionResponse>(GetSectionPathWithCategory(categoryId));
         }
 
         public IndividualSectionResponse GetSectionById(long id)
         {
-            return GenericGet<IndividualSectionResponse>($"help_center/sections/{id}.json?include=access_policies");
+            return GenericGet<IndividualSectionResponse>($"{GeneralSectionsPath}/{id}.json?include=access_policies");
         }
 
         public IndividualSectionResponse CreateSection(Section section)
         {
             var body = new { section };
-            return GenericPost<IndividualSectionResponse>($"help_center/categories/{section.CategoryId}/sections.json?include=access_policies", body);
+            return GenericPost<IndividualSectionResponse>(GetSectionPathWithCategory(section.CategoryId), body);
         }
 
         public IndividualSectionResponse UpdateSection(Section section)
         {
             var body = new { section };
-            return GenericPut<IndividualSectionResponse>($"help_center/sections/{section.Id}.json?include=access_policies", body);
+            return GenericPut<IndividualSectionResponse>($"{GeneralSectionsPath}/{section.Id}.json?include=access_policies", body);
         }
 
         public bool DeleteSection(long id)
@@ -77,29 +84,29 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 
         public async Task<GroupSectionResponse> GetSectionsAsync()
         {
-            return await GenericGetAsync<GroupSectionResponse>("help_center/sections.json?include=access_policies");
+            return await GenericGetAsync<GroupSectionResponse>($"{GeneralSectionsPath}.json?include=access_policies");
         }
 
         public async Task<GroupSectionResponse> GetSectionsByCategoryIdAsync(long categoryId)
         {
-            return await GenericGetAsync<GroupSectionResponse>($"help_center/categories/{categoryId}/sections.json?include=access_policies");
+            return await GenericGetAsync<GroupSectionResponse>(GetSectionPathWithCategory(categoryId));
         }
 
         public async Task<IndividualSectionResponse> GetSectionByIdAsync(long id)
         {
-            return await GenericGetAsync<IndividualSectionResponse>($"help_center/sections/{id}.json?include=access_policies");
+            return await GenericGetAsync<IndividualSectionResponse>($"{GeneralSectionsPath}/{id}.json?include=access_policies");
         }
 
         public async Task<IndividualSectionResponse> CreateSectionAsync(Section section)
         {
             var body = new { section };
-            return await GenericPostAsync<IndividualSectionResponse>($"help_center/categories/{section.CategoryId}/sections.json?include=access_policies", body);
+            return await GenericPostAsync<IndividualSectionResponse>(GetSectionPathWithCategory(section.CategoryId), body);
         }
 
         public async Task<IndividualSectionResponse> UpdateSectionAsync(Section section)
         {
             var body = new { section };
-            return await GenericPutAsync<IndividualSectionResponse>($"help_center/sections/{section.Id}.json?include=access_policies", body);
+            return await GenericPutAsync<IndividualSectionResponse>($"{GeneralSectionsPath}/{section.Id}.json?include=access_policies", body);
         }
 
         public async Task<bool> DeleteSectionAsync(long id)
@@ -108,5 +115,12 @@ namespace ZendeskApi_v2.Requests.HelpCenter
         }
 
 #endif
+
+        private string GetSectionPathWithCategory(long? categoryId)
+        {
+            return !string.IsNullOrWhiteSpace(Locale)
+                ? $"help_center/{Locale}/categories/{categoryId}/sections.json?include=access_policies"
+                : $"help_center/categories/{categoryId}/sections.json?include=access_policies";
+        }
     }
 }
