672ed15a6f13e4e00140020f914fadd37937e59a
Rasmus Wulff Jensen <login@rwj.dk>
2018-06-22 22:50:11 +0200

    Issue #330 + CRUD operations for Comments (#344)
    * Test "CanCreateTicketWithDueDate" can't run on PCs that use dd/MM/yyyy as time format
    
    * Added Code to make tests work
    
    * - Added support for issue #330
    - Added support for CRUD operations of Comments on Articles and Posts

--- src/ZendeskApi_v2/Requests/HelpCenter/Comments.cs
+++ src/ZendeskApi_v2/Requests/HelpCenter/Comments.cs
@@ -1,8 +1,6 @@
-﻿using System;
-#if ASYNC
+﻿#if ASYNC
 using System.Threading.Tasks;
 #endif
-using ZendeskApi_v2.Extensions;
 using ZendeskApi_v2.Models.HelpCenter.Comments;
 
 namespace ZendeskApi_v2.Requests.HelpCenter
@@ -14,14 +12,29 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 		GroupCommentResponse GetCommentsForArticle(long? articleId, int? perPage = null, int? page = null);
         GroupCommentResponse GetCommentsForUser(long? userId);
         GroupCommentResponse GetCommentsForCurrentUser();
+	    GroupCommentResponse GetCommentsForPost(long? postId, int? perPage = null, int? page = null);
+	    IndividualCommentsResponse CreateCommentForArticle(long articleId, Comment comment);
+	    IndividualCommentsResponse CreateCommentForPost(long postId, Comment comment);
+	    IndividualCommentsResponse UpdateCommentForArticle(long articleId, Comment comment);
+	    IndividualCommentsResponse UpdateCommentForPost(long postId, Comment comment);
+        bool DeleteCommentForArticle(long articleId, long commentId);
+        bool DeleteCommentForPost(long postId, long commentId);
 #endif
 #if ASYNC
-		Task<GroupCommentResponse> GetCommentsForArticleAsync(long? articleId);
+        Task<GroupCommentResponse> GetCommentsForArticleAsync(long? articleId);
         Task<GroupCommentResponse> GetCommentsForUserAsync(long? userId);
         Task<GroupCommentResponse> GetCommentsForCurrentUserAsync();
+	    Task<GroupCommentResponse> GetCommentsForPostAsync(long? postId);
+        Task<IndividualCommentsResponse> CreateCommentForArticleAsync(long articleId, Comment comment);
+	    Task<IndividualCommentsResponse> CreateCommentForPostAsync(long postId, Comment comment);
+	    Task<IndividualCommentsResponse> UpdateCommentForArticleAsync(long articleId, Comment comment);
+	    Task<IndividualCommentsResponse> UpdateCommentForPostAsync(long postId, Comment comment);
+	    Task<bool> DeleteCommentForArticleAsync(long articleId, long commentId);
+	    Task<bool> DeleteCommentForPostAsync(long postId, long commentId);
 #endif
 
-	}
+
+    }
 
     /// <summary>
     /// https://developer.zendesk.com/rest_api/docs/help_center/comments
@@ -49,6 +62,45 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 		{
 			return GenericGet<GroupCommentResponse>("help_center/users/me/comments.json");
 		}
+
+	    public GroupCommentResponse GetCommentsForPost(long? postId, int? perPage = null, int? page = null)
+	    {
+	        return GenericPagedGet<GroupCommentResponse>($"community/posts/{postId}/comments.json", perPage, page);
+        }
+
+	    public IndividualCommentsResponse CreateCommentForArticle(long articleId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return GenericPost<IndividualCommentsResponse>($"help_center/articles/{articleId}/comments.json", body);
+        }
+        
+	    public IndividualCommentsResponse CreateCommentForPost(long postId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return GenericPost<IndividualCommentsResponse>($"community/posts/{postId}/comments.json", body);
+        }
+
+	    public IndividualCommentsResponse UpdateCommentForArticle(long articleId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return GenericPut<IndividualCommentsResponse>($"help_center/articles/{articleId}/comments/{comment.Id}.json", body);
+        }
+
+	    public IndividualCommentsResponse UpdateCommentForPost(long postId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return GenericPut<IndividualCommentsResponse>($"community/posts/{postId}/comments/{comment.Id}.json", body);
+        }
+
+	    public bool DeleteCommentForArticle(long articleId, long commentId)
+	    {
+	        return GenericDelete($"help_center/articles/{articleId}/comments/{commentId}.json");
+        }
+
+	    public bool DeleteCommentForPost(long postId, long commentId)
+	    {
+	        return GenericDelete($"community/posts/{postId}/comments/{commentId}.json");
+        }
 #endif
 #if ASYNC
 
@@ -66,6 +118,45 @@ namespace ZendeskApi_v2.Requests.HelpCenter
 		{
 			return await GenericGetAsync<GroupCommentResponse>("help_center/users/me/comments.json");
 		}
+
+	    public async Task<GroupCommentResponse> GetCommentsForPostAsync(long? postId)
+	    {
+	        return await GenericGetAsync<GroupCommentResponse>($"community/posts/{postId}/comments.json");
+        }
+
+	    public async Task<IndividualCommentsResponse> CreateCommentForArticleAsync(long articleId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return await GenericPostAsync<IndividualCommentsResponse>($"help_center/articles/{articleId}/comments.json", body);
+        }
+
+	    public async Task<IndividualCommentsResponse> CreateCommentForPostAsync(long postId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return await GenericPostAsync<IndividualCommentsResponse>($"community/posts/{postId}/comments.json", body);
+        }
+
+	    public async Task<IndividualCommentsResponse> UpdateCommentForArticleAsync(long articleId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return await GenericPutAsync<IndividualCommentsResponse>($"help_center/articles/{articleId}/comments/{comment.Id}.json", body);
+        }
+
+	    public async Task<IndividualCommentsResponse> UpdateCommentForPostAsync(long postId, Comment comment)
+	    {
+	        var body = new { comment };
+	        return await GenericPutAsync<IndividualCommentsResponse>($"community/posts/{postId}/comments/{comment.Id}.json", body);
+        }
+
+	    public async Task<bool> DeleteCommentForArticleAsync(long articleId, long commentId)
+	    {
+	        return await GenericDeleteAsync($"help_center/articles/{articleId}/comments/{commentId}.json");
+        }
+
+	    public async Task<bool> DeleteCommentForPostAsync(long postId, long commentId)
+	    {
+	        return await GenericDeleteAsync($"community/posts/{postId}/comments/{commentId}.json");
+        }
 #endif
 	}
 }
