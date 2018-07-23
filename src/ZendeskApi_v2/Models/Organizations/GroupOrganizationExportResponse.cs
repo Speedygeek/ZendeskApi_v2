611159a183d7a5c56b3726b5f498006f78d0be24
Ben Emmett <bcemmett@users.noreply.github.com>
2018-07-23 01:51:32 +0100

    Support incremental org exports

---
+++ src/ZendeskApi_v2/Models/Organizations/GroupOrganizationExportResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ZendeskApi_v2.Models.Organizations
+{
+    public class GroupOrganizationExportResponse
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("end_time")]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTimeOffset EndTime { get; set; }
+
+        [JsonProperty("next_page")]
+        public string NextPage { get; set; }
+
+        [JsonProperty("organizations")]
+        public IList<Organization> Organizations { get; set; }
+    }
+}
