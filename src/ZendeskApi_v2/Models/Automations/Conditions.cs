214df27b836e31c7a53dbe23b3fa70e2b4b83308
cgray4263 <44887504+cgray4263@users.noreply.github.com>
2019-07-11 09:06:32 -0400

    Adding support for automations and ticketform conditions (#440)
    * Add model for Automations.  Bring TicketForms model up to date with online API docs (adds branding items, conditional fields, and raw names).
    
    * Remove 'raw' properties from TicketForm, Add Automation unit tests.
    
    * Adding SharedPhoneNumber to User model
    add fix for #441

---
+++ src/ZendeskApi_v2/Models/Automations/Conditions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.Automations
+{
+    public class Conditions
+    {
+        [JsonProperty("all")]
+        public IList<All> All { get; set; }
+
+        [JsonProperty("any")]
+        public IList<All> Any { get; set; }
+    }
+}
---
+++ src/ZendeskApi_v2/Models/Automations/GroupAutomationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.Automations
+{
+    public class GroupAutomationResponse : GroupResponseBase
+    {
+        [JsonProperty("automations")]
+        public IList<Automation> Automations { get; set; }
+    }
+}
