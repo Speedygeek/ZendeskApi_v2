db1e2944ffd238641bbdb185c3bec5251b8976d8
Tim Nilimaa-Svärd <TimNilimaa@users.noreply.github.com>
2019-08-20 14:40:17 +0200

    Added initial support Talk Account Overview (#451)
    Added feature to get data for the non Pro/Ent versions

---
+++ src/ZendeskApi_v2/Models/Voice/AccountOverview.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.Voice
+{
+    public class AccountOverview
+    {
+        [JsonProperty("average_call_duration")]
+        public long AverageCallDuration { get; set; }
+
+        [JsonProperty("average_queue_wait_time")]
+        public long AverageQueueWaitTime { get; set; }
+
+        [JsonProperty("average_wrap_up_time")]
+        public long AverageWarpUpTime { get; set; }
+
+        [JsonProperty("max_calls_waiting")]
+        public long MaxCallsWaiting { get; set; }
+
+        [JsonProperty("max_queue_wait_time")]
+        public long MaxQueueWaitTime { get; set; }
+
+        [JsonProperty("total_call_duration")]
+        public long TotalCallDuration { get; set; }
+
+        [JsonProperty("total_calls")]
+        public long TotalCalls { get; set; }
+
+        [JsonProperty("total_voicemails")]
+        public long TotalVoicemails { get; set; }
+
+        [JsonProperty("total_wrap_up_time")]
+        public long TotalWrapUpTime { get; set; }
+    }
+}
