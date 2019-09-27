8236d65642110b416433f2928fb7e58f48e7a59d
Elizabeth Schneider <elizabeth@speedy-geek.com>
2019-09-26 22:21:06 -0600

    fixed nuget github urls (#456)

---
+++ tools/CreateRelease.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    $VersionNumber = "3.10.4"
+
+)
+
+git checkout master
+git pull
+
+git tag -s "v$($VersionNumber)" -m "v$($VersionNumber)"
+
+git push --tags
