2c266a7640c081d82dbb3cac0f81ff9b7fef4d6b
Laura del Pino Díaz <lauradelpinodiaz94@gmail.com>
2019-10-26 13:35:46 +0100

    Added Create or Update (#461)
    * Added Create or Update
    
    * Added Create or Update async version and unit test for sync and async version
    
    * Fixed codefactor issue

--- src/ZendeskApi_v2/Requests/Users.cs
+++ src/ZendeskApi_v2/Requests/Users.cs
@@ -64,6 +64,8 @@ namespace ZendeskApi_v2.Requests
 
         IndividualUserResponse CreateUser(User user);
 
+        IndividualUserResponse CreateOrUpdateUser(User user);
+
         JobStatusResponse BulkCreateUsers(IEnumerable<User> users);
 
         JobStatusResponse BulkUpdate(IEnumerable<long> ids, User user);
@@ -151,6 +153,7 @@ namespace ZendeskApi_v2.Requests
         Task<GroupUserResponse> GetUsersInOrganizationAsync(long id, int? perPage = null, int? page = null, UserSideLoadOptions sideLoadOptions = UserSideLoadOptions.None);
 
         Task<IndividualUserResponse> CreateUserAsync(User user);
+        Task<IndividualUserResponse> CreateOrUpdateUserAsync(User user);
 
         Task<JobStatusResponse> BulkCreateUsersAsync(IEnumerable<User> users);
 
@@ -340,6 +343,11 @@ namespace ZendeskApi_v2.Requests
         public IndividualUserResponse CreateUser(User user)
         {
             return GenericPost<IndividualUserResponse>("users.json", new { user });
+        }
+
+        public IndividualUserResponse CreateOrUpdateUser(User user)
+        {
+            return GenericPost<IndividualUserResponse>($"users/create_or_update.json", new { user });
         }
 
         public JobStatusResponse BulkCreateUsers(IEnumerable<User> users)
@@ -578,6 +586,11 @@ namespace ZendeskApi_v2.Requests
         public async Task<IndividualUserResponse> CreateUserAsync(User user)
         {
             return await GenericPostAsync<IndividualUserResponse>("users.json", new { user });
+        }
+
+        public async Task<IndividualUserResponse> CreateOrUpdateUserAsync(User user)
+        {
+            return await GenericPostAsync<IndividualUserResponse>($"users/create_or_update.json", new { user });
         }
 
         public async Task<JobStatusResponse> BulkCreateUsersAsync(IEnumerable<User> users)
