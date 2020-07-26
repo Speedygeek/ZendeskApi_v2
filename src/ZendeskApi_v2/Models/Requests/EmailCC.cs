453ca72383fd3025f97312f2e680bf7333b7673e
George Vasileiadis <georgevassiliadis@hotmail.com>
2020-07-26 17:55:37 +0200

    Extend Request model to accept email ccs (#486)

---
+++ src/ZendeskApi_v2/Models/Requests/EmailCC.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.Tickets
+{
+    public class EmailCC
+    {
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("user_email")]
+        public string UserEmail { get; set; }
+
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+    }
+}
